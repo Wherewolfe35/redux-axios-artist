0a18c7d9973a412d686827916389b652994d07f8
Christopher Black <chris@primeacademy.io>
2019-01-23 13:50:04 -0600

    Added redux start.

--- server/routes/artist.router.js
+++ server/routes/artist.router.js
@@ -5,11 +5,25 @@ const router = express.Router();
 // Using a array of data on the server, we will eventually
 // move this back into the database.
 const artists = require('../modules/artist.data');
+let nextId = artists.length;
 
-router.delete('/:id', (req, res) => {
-    artists.splice(req.params.id, 1);
+router.delete('/:id', (req, res) => {    
+    // TODO: Use filter to remove the artist
+    // artists = artists.filter(...)
+
     res.sendStatus(200);
-})
+});
+
+// GET all the books
+router.post('/', (req, res) => {
+    console.log('In artist POST with', req.body);
+    const artistToAdd = req.body;
+    // add an id to the incoming artist
+    artistToAdd.id = nextId;
+    nextId += 1;
+    artists.push(artistToAdd);
+    res.send(201);
+}); // END GET Route
 
 // GET all the books
 router.get('/', (req, res) => {
